880937ce469358511649d6f9aff3c9b80bf4877b
AnaJL <analinsdev@gmail.com>
2020-05-14 10:22:33 -0300

    cadastro com ReactJS

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Main from "../template/Main"
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Segundo projeto de ReactJS">
+    <div className="display-4">Bem Vindo !</div>
+    <hr />
+    <p className="mb-0">Sistema para exemplificar a construão de um cadastro com ReactJS </p>
+    </Main>
---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "font-awesome/css/font-awesome.min.css"
+import React from "react"
+
+import { BrowserRouter } from 'react-router-dom'  // Use HashRouter or BrowserRouter
+import Routes from "./Routes"
+
+import Logo from "../components/template/Logo"
+import Nav from "../components/template/Nav"
+import Footer from "../components/template/Footer"
+import Home from "../components/home/Home"
+
+
+
+export default props =>
+<BrowserRouter>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Routes />
+        <Footer />
+    </div>
+</BrowserRouter>
